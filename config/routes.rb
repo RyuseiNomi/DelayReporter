c147b4cf51528e4373120a4de012ed0d9f4e8980
RyuseiNomi <kanontonto1010@gmail.com>
2020-06-16 23:33:58 +0900

    Add router to about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
   get 'search/index' => 'search#index'
   get 'delay_lists/index' => 'delay_lists/index'
-  get '/' => 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
